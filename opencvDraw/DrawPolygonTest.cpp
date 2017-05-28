e2e0f7ee09712b2cc54e3564959205cd3f2e00f6
lgdlkq <1732109940@qq.com>
2017-05-28 21:04:33 +0800

    添加DrawPolygon()函数的写法

---
+++ opencvDraw/DrawPolygonTest.cpp
@@ -0,0 +1,39 @@
+#include"all.h"
+
+//自定义绘制函数，实现了凹多边形的绘制
+void DrawPolygon(Mat img) {
+	int lineType = 8;
+
+	//创建一些点
+	Point rookPoints[1][20];
+	rookPoints[0][0] = Point(WINDOW_WIDTH / 4, 7 * WINDOW_WIDTH / 8);
+	rookPoints[0][1] = Point(3 * WINDOW_WIDTH / 4, 7 * WINDOW_WIDTH / 8);
+	rookPoints[0][2] = Point(3 * WINDOW_WIDTH / 4, 13 * WINDOW_WIDTH / 16);
+	rookPoints[0][3] = Point(11 * WINDOW_WIDTH / 16, 7 * WINDOW_WIDTH / 16);
+	rookPoints[0][4] = Point(19 * WINDOW_WIDTH / 32, 3 * WINDOW_WIDTH / 8);
+	rookPoints[0][5] = Point(3 * WINDOW_WIDTH / 4, 7 * WINDOW_WIDTH / 8);
+	rookPoints[0][6] = Point(3 * WINDOW_WIDTH / 4, WINDOW_WIDTH / 8);
+	rookPoints[0][7] = Point(26 * WINDOW_WIDTH / 40, WINDOW_WIDTH / 8);
+	rookPoints[0][8] = Point(26 * WINDOW_WIDTH / 40, WINDOW_WIDTH / 4);
+	rookPoints[0][9] = Point(22 * WINDOW_WIDTH / 40, WINDOW_WIDTH / 8);
+	rookPoints[0][10] = Point(22 * WINDOW_WIDTH / 40, WINDOW_WIDTH / 8);
+	rookPoints[0][11] = Point(18 * WINDOW_WIDTH / 40, WINDOW_WIDTH / 8);
+	rookPoints[0][12] = Point(18 * WINDOW_WIDTH / 40, WINDOW_WIDTH / 4);
+	rookPoints[0][13] = Point(14 * WINDOW_WIDTH / 40, WINDOW_WIDTH / 4);
+	rookPoints[0][14] = Point(14 * WINDOW_WIDTH / 40, 7 * WINDOW_WIDTH / 8);
+	rookPoints[0][15] = Point(WINDOW_WIDTH / 4, WINDOW_WIDTH / 8);
+	rookPoints[0][16] = Point(WINDOW_WIDTH / 4, WINDOW_WIDTH / 8);
+	rookPoints[0][17] = Point(13 * WINDOW_WIDTH / 32, 3 * WINDOW_WIDTH / 8);
+	rookPoints[0][18] = Point(5 * WINDOW_WIDTH / 16, 13 * WINDOW_WIDTH / 16);
+	rookPoints[0][19] = Point(WINDOW_WIDTH / 4, 13 * WINDOW_WIDTH / 16);
+
+	const Point* ppt[1] = { rookPoints[0] };
+	int npt[] = { 20 };
+
+	fillPoly(img,
+		ppt,
+		npt,
+		1,
+		Scalar(255, 255, 255),
+		lineType);
+}
